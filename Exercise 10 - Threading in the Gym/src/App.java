8caae4b13ae4f42af82ede78242c05f1facce6bf
Joyce Lau <jshizuki@yahoo.com.hk>
2025-02-14 17:50:02 +0900

    Initial Commit with all classes and core methods set up

---
+++ Exercise 10 - Threading in the Gym/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      // Weight weight = Weight.getRandomWeight();
+      // System.out.println(weight);
+      // System.out.println(Weight.numOfWeightsInGym.get(weight));
+
+      // MachineType machine = MachineType.getRandomEquipment();
+      // System.out.println(machine);
+      // System.out.println(machine.machineName);
+
+      // Exercise exercise1 = new Exercise();
+      // System.out.println(exercise1);
+
+      Member member1 = new Member(1);
+      member1.performRoutine();
+    }
+}
---
+++ Exercise 10 - Threading in the Gym/src/Gym.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Gym {
+  private final int totalGymMembers;
+  private Map<MachineType, Integer> availableMachines;
+
+  public Gym(int totalGymMembers, Map<MachineType, Integer> availableMachines) {
+    this.totalGymMembers = totalGymMembers;
+    this.availableMachines = availableMachines;
+  }
+
+  public void openForToday() {
+    List<Thread> gymMembersRoutines;
+
+    gymMembersRoutines = IntStream.rangeClosed(1, this.totalGymMembers)
+        .mapToObj(id -> {
+          Member member = new Member(id);
+          return new Thread(() -> {
+            try {
+              member.performRoutine();
+            } catch (Exception e) {
+              System.out.println(e);
+            }
+          });
+        }).collect(Collectors.toList());
+  }
+}
